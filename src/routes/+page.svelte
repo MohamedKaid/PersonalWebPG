f05d352cc1971fcb7d7e5be04d46883130dc107a
MohamedKaid <mohamedkaid014@gmail.com>
2025-03-01 17:37:24 -0500

    added a few thing to the page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,25 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let title = "Hi, my name is Mohamed Kaid";
+</script>
+
+<style>
+    @keyframes gradientAnimation {
+        0% { background-position: 0% 50%; }
+        50% { background-position: 100% 50%; }
+        100% { background-position: 0% 50%; }
+    }
+
+    .animated-bg {
+        background: linear-gradient(270deg, #3b82f6, #9333ea, #f43f5e, #f59e0b);
+        background-size: 400% 400%;
+        animation: gradientAnimation 6s ease infinite;
+    }
+</style>
+
+<main class="flex flex-col justify-center items-center h-screen animated-bg text-white">
+    <img src="" alt="">
+    <h1 class="text-4xl font-bold">{title}</h1>
+</main>
+
+
+
